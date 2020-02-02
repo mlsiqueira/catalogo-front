cb9b85a656bea95634465e1f673fc5d6c67d2353
Mauro Siqueira <mlsiqueira.whs@outlook.com>
2020-02-02 10:02:00 -0300

    criação do scomponente movies

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { DataService } from 'src/app/services/data.service';
+import { Movie } from 'src/app/models/types';
 
 @Component({
   selector: 'app-movies',
@@ -8,10 +11,12 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class MoviesComponent implements OnInit {
 
+  movies$: Observable<Movie[]>;
+
   constructor(private data: DataService) { }
 
   ngOnInit() {
-    this.data.listMovies().subscribe(console.log);
+    this.movies$ = this.data.listMovies();
   }
 
 }
